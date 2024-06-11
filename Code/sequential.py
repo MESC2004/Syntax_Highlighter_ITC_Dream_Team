3facb6b18fb872f01527ef89456225e7620ad881
BassedWarrior <Fausto_Jimenez_Vallejo@outlook.com>
2024-06-11 00:53:30 -0600

    Generate `.html` files, not `.py.html` files now

--- Code/sequential.py
+++ Code/sequential.py
@@ -21,7 +21,7 @@ def get_files(path):
                 get_files(os.path.join(path, fichero.name))
 
             elif fichero.is_file() and fichero.name.endswith(".py"):
-                f2 = open(f"{fichero.name}.html", "w")
+                f2 = open(f"{fichero.name.removesuffix('.py')}.html", "w")
                 f2.write(lexer(os.path.join(path, fichero.name)))
                 f2.close()
 
