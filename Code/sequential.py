6fbb2747cf30808b7e3100973704d6323776425a
JoseGlez <A01028517@tec.mx>
2024-06-10 23:44:09 -0600

    Sequential code for finding .py documents

--- Code/sequential.py
+++ Code/sequential.py
@@ -7,3 +7,28 @@ un cierto PATH dado al script.
 Alternativamente, se puede utilizar como módulo y llamarse desde un script
 externo.
 """
+import os
+import time
+from lexer import lexer
+
+ejemplo_dir = '/Users/jagle/OneDrive/Escritorio/Directorios/Syntax_Highlighter_ITC_Dream_Team/Code/Test_Code'
+def get_files(path):
+   
+    with os.scandir(path) as ficheros:
+        for fichero in ficheros:
+            
+            if (fichero.is_dir()):
+                get_files(os.path.join(path, fichero.name))
+                
+            elif (fichero.is_file() and fichero.name.endswith('.py')):
+                
+                f2=open(f"{fichero.name}.html","w")
+                f2.write(lexer(os.path.join(path, fichero.name)))
+                f2.close()
+                
+if __name__=="__main__":
+    ti=time.perf_counter()
+    get_files(ejemplo_dir)
+    tf=time.perf_counter()
+    print(f"Time elapsed: {tf-ti} seconds")
+    
