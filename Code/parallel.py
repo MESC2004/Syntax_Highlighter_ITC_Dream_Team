d89de84324df41235b470d5517ac753cedd3033a
BassedWarrior <116653989+BassedWarrior@users.noreply.github.com>
2024-06-11 10:44:26 -0600

    Copied `sequential.py` and added `threading` import
    Both files and implementations will be fairly similar, so a good starting point is to just copy what was implemented in `sequential.py`.

--- Code/parallel.py
+++ Code/parallel.py
@@ -7,3 +7,32 @@ cierto PATH dado al script.
 Alternativamente, se puede utilizar como módulo y llamarse desde un script
 externo.
 """
+
+import os
+import time
+import threading
+from lexer import lexer
+
+
+def get_files(path):
+    """Recursivamente navegar el path y resaltar los archivos .py"""
+    with os.scandir(path) as ficheros:
+        for fichero in ficheros:
+            if fichero.is_dir():
+                get_files(os.path.join(path, fichero.name))
+
+            elif fichero.is_file() and fichero.name.endswith(".py"):
+                f2 = open(f"{fichero.name.removesuffix('.py')}.html", "w")
+                f2.write(lexer(os.path.join(path, fichero.name)))
+                f2.close()
+
+
+def main():
+    ti = time.perf_counter()
+    get_files(os.path.join(os.getcwd(), "Test_Code"))
+    tf = time.perf_counter()
+    print(f"Time elapsed: {tf-ti} seconds")
+
+
+if __name__ == "__main__":
+    main()
