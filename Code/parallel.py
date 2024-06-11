9a0fa39a8db7fb72f2e703d3a1ba556f7faff639
BassedWarrior <Fausto_Jimenez_Vallejo@outlook.com>
2024-06-10 20:59:10 -0600

    Split functionality into modules
    - **`lexer.py`**: DFA definition and individual file lexing.
    - **`parallel.py`**: Parallel implementation of `lexer.py` for
      directories.
    - **`sequential.py`**: Sequential implementation of `lexer.py` for
      directories.

---
+++ Code/parallel.py
@@ -0,0 +1,9 @@
+"""Implementación paralela del lexer
+
+Este archivo contiene la implementación paralela del lexer. Está diseñado para
+correrse como un script que analice los archivos .py contenidos dentro de un
+cierto PATH dado al script.
+
+Alternativamente, se puede utilizar como módulo y llamarse desde un script
+externo.
+"""
