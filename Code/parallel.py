b52a125321f358236c3baa7b020c6b986ec19ceb
BassedWarrior <Fausto_Jimenez_Vallejo@outlook.com>
2024-06-11 11:43:48 -0600

    Implemented multithreading. Currently slower than sequential

--- Code/parallel.py
+++ Code/parallel.py
@@ -14,17 +14,40 @@ import threading
 from lexer import lexer
 
 
+def parse_file(path):
+    """Resaltar un archivo produciendo un .html que contenga el formato."""
+    with open(f"{path.removesuffix('.py')}.html", "w") as f2:
+        f2.write(lexer(path))
+        f2.close()
+
+
 def get_files(path):
     """Recursivamente navegar el path y resaltar los archivos .py"""
     with os.scandir(path) as ficheros:
+        hilos = []
         for fichero in ficheros:
             if fichero.is_dir():
-                get_files(fichero.path)
+                hilos.append(
+                    threading.Thread(
+                        name=f"get_files:{fichero}",
+                        target=get_files,
+                        args=(fichero.path,),
+                    )
+                )
+                hilos[-1].start()
 
             elif fichero.is_file() and fichero.name.endswith(".py"):
-                f2 = open(f"{fichero.path.removesuffix('.py')}.html", "w")
-                f2.write(lexer(fichero.path))
-                f2.close()
+                hilos.append(
+                    threading.Thread(
+                        name=f"parse_file:{fichero}",
+                        target=parse_file,
+                        args=(fichero.path,),
+                    )
+                )
+                hilos[-1].start()
+
+        for hilo in hilos:
+            hilo.join()
 
 
 def main():
